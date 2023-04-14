0aff4cc80e339244dc270144d15da8ca7b846ec0
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-15 00:10:58 +0530

    ecommerce task 6: fetch api with async await

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function clickHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    setMovies(
+      data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      })
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={clickHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
