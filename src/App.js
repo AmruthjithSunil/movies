75bb14d167e2c39fd03b5f55170ef89b60ef73f4
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-15 00:58:16 +0530

    ecommerce task 8: added retrying and canceling request

--- src/App.js
+++ src/App.js
@@ -6,22 +6,46 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [cancel, setCancel] = useState(false);
 
   async function clickHandler() {
+    if (cancel) {
+      console.log("cancelled");
+      setCancel(false);
+      setError(null);
+      return;
+    }
+    console.log("clicked");
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    setMovies(
-      data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-      })
-    );
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Something went wrong ...Retrying");
+      }
+      const data = await response.json();
+      setMovies(
+        data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl,
+            releaseDate: movie.release_date,
+          };
+        })
+      );
+    } catch (err) {
+      setError(err.message);
+    }
     setIsLoading(false);
+    if (error) {
+      setInterval(clickHandler, 5000);
+    }
+  }
+
+  function cancelHandler() {
+    setCancel(true);
   }
 
   return (
@@ -31,6 +55,12 @@ function App() {
       </section>
       <section>
         {isLoading && <>Loading...</>}
+        {error && (
+          <>
+            {error}
+            <button onClick={cancelHandler}>Cancel</button>
+          </>
+        )}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
